c619aad65d3e7d1b7420f8282c9f6f621d002d1c
jimmybuuur <burkleyj122@gmail.com>
2023-05-11 00:51:51 -0700

    improved chat ui

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -94,24 +94,127 @@ function Chat() {
     //   </ul>  
     // </div>
     
-    <div style={{maxWidth: '400px', margin: 'auto'}}>  
-    <div style={{background: '#f1f0f0', padding: '10px', borderBottom: '1px solid #ccc'}}>  
-        <h2 style={{margin: '0'}}>Chat</h2>  
-    </div>  
-    <div style={{height: '400px', overflowY: 'scroll'}}>  
-        {messages.map((message, index) => (  
-        <div key={index} style={{display: 'flex', justifyContent: message.user_id === 'Me' ? 'flex-end' : 'flex-start', margin: '10px'}}>  
-            <div style={{background: message.user_id === 'Me' ? '#DCF8C6' : '#fff', padding: '10px', borderRadius: '10px', maxWidth: '70%'}}>  
-            {message.message}  
-            </div>  
-        </div>  
-        ))}  
-    </div>  
-    <form onSubmit={handleSubmit} style={{display: 'flex', marginTop: '10px'}}>  
-        <input type="text" value={newMessage} onChange={(event) => setNewMessage(event.target.value)} style={{flex: '1', padding: '10px'}} />  
-        <button type="submit" style={{background: '#4CAF50', color: '#fff', padding: '10px', border: 'none', borderRadius: '0 10px 10px 0'}}>Send</button>  
-    </form>  
-    </div> 
+
+
+    // <div style={{maxWidth: '400px', margin: 'auto'}}>  
+    // <div style={{background: '#f1f0f0', padding: '10px', borderBottom: '1px solid #ccc'}}>  
+    //     <h2 style={{margin: '0'}}>Chat</h2>  
+    // </div>  ''
+    // <div style={{height: '400px', overflowY: 'scroll'}}>  
+    //     {messages.map((message, index) => (  
+    //     <div key={index} style={{display: 'flex', justifyContent: message.user_id === 'Me' ? 'flex-end' : 'flex-start', margin: '10px'}}>  
+    //         <div style={{background: message.user_id === 'Me' ? '#DCF8C6' : '#fff', padding: '10px', borderRadius: '10px', maxWidth: '70%'}}>  
+    //         {message.message}  
+    //         </div>  
+    //     </div>  
+    //     ))}  
+    // </div>  
+    // <form onSubmit={handleSubmit} style={{display: 'flex', marginTop: '10px'}}>  
+    //     <input type="text" value={newMessage} onChange={(event) => setNewMessage(event.target.value)} style={{flex: '1', padding: '10px'}} />  
+    //     <button type="submit" style={{background: '#4CAF50', color: '#fff', padding: '10px', border: 'none', borderRadius: '0 10px 10px 0'}}>Send</button>  
+    // </form>  
+    // </div> 
+
+        <html>
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Chat</title>
+            <style>
+            body {
+                margin: 0;
+                padding: 0;
+                font-family: sans-serif;
+            }
+
+            .chat-container {
+                max-width: 400px;
+                margin: auto;
+            }
+
+            .chat-header {
+                background: #f1f0f0;
+                padding: 10px;
+                border-bottom: 1px solid #ccc;
+            }
+
+            .chat-header h2 {
+                margin: 0;
+            }
+
+            .chat-messages {
+                height: 400px;
+                overflow-y: scroll;
+            }
+
+            .chat-message {
+                display: flex;
+                justify-content: flex-end;
+                margin: 10px;
+            }
+
+            .chat-message-other {
+                justify-content: flex-start;
+            }
+
+            .chat-message-bubble {
+                background-color: #DCF8C6;
+                padding: 10px;
+                border-radius: 10px;
+                max-width: 70%;
+            }
+
+            .chat-message-bubble-other {
+                background-color: #fff;
+            }
+
+            .chat-input-form {
+                display: flex;
+                margin-top: 10px;
+            }
+
+            .chat-input-form input[type="text"] {
+                flex: 1;
+                padding: 10px;
+            }
+
+            .chat-input-form button[type="submit"] {
+                background-color: #4CAF50;
+                color: #fff;
+                padding: 10px;
+                border: none;
+                border-radius: 0 10px 10px 0;
+            }
+            </style>
+        </head>
+
+        <body>
+            <div class="chat-container">
+            <div class="chat-header">
+                <h2>Chat</h2>
+            </div>
+
+            <div class="chat-messages">
+                <div class="chat-message">
+                <div class="chat-message-bubble">Hola!</div>
+                </div>
+            </div>
+
+            <form class="chat-input-form" onSubmit={handleSubmit}>
+                <input
+                type="text"
+                value={newMessage}
+                onChange={(event) => setNewMessage(event.target.value)}
+                placeholder="Escribe tu mensaje aqui..."
+                required
+                />
+                
+                <button type="submit">Send</button>
+                
+            </form>
+            </div>
+        </body>
+        </html>
   );  
 }  
   
