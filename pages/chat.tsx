2f8395c7ebcc806a1cc2f6ff362d29d93cba39da
jimmybuuur <burkleyj122@gmail.com>
2023-05-11 13:03:24 -0700

    chat page

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -53,67 +53,60 @@ function Chat() {
     }  
   };  
   
-  const handleSubmit =  async (event: React.FormEvent<HTMLFormElement>) => {  
-    event.preventDefault();  
+//   const handleSubmit =  async (event: React.FormEvent<HTMLFormElement>) => {  
+//     event.preventDefault();  
+//     //log user.id to console
+//     console.log('user.id: ', userDetails?.id);
+//     if (!newMessage.trim()) {  
+//       return;  
+//     }  
   
+//     const { data: message, error } = await supabase.from('messages').insert([    
+//         { message: newMessage, user_id: user?.id }    
+//       ]);         
+  
+//     if (error) {  
+//       console.log('Error creating message:', error.message);  
+//     } else {  
+//         if (message) {  
+//             setMessages([message, ...messages]);  
+//             setNewMessage('');  
+//         }  
+//     }  
+//   };  
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {  
+    event.preventDefault();  
+    console.log('user.id: ', userDetails?.id);  
     if (!newMessage.trim()) {  
       return;  
     }  
-  
-    const { data: message, error } = await supabase.from('messages').insert([    
-        { message: newMessage, user_id: user?.id }    
-      ]);         
-  
+    
+    const { data: message, error } = await supabase  
+      .from('messages')  
+      .insert([{ message: newMessage, user_id: user?.id }]);  
+    
     if (error) {  
       console.log('Error creating message:', error.message);  
     } else {  
-        if (message) {  
-            setMessages([message, ...messages]);  
-            setNewMessage('');  
-        }  
+      if (message) {  
+        const formattedMessage = {  
+          id: message[0].id,  
+          message: message[0].message,  
+          user_id: message[0].user_id,  
+          created_at: message[0].created_at,  
+        };  
+        setMessages([formattedMessage, ...messages]);  
+        setNewMessage('');  
+      }  
     }  
   };  
   
+  // log message.user_id and userDetails?.id to console
+    console.log('message.user_id: ', message.user_id);
+    console.log('userDetails?.id: ', userDetails?.id);
+
   return (  
-    // <div>  
-    //   <h1>Chat</h1>  
-    //   <form onSubmit={handleSubmit}>  
-    //     <input  
-    //       type="text"  
-    //       value={newMessage}  
-    //       onChange={(event) => setNewMessage(event.target.value)}  
-    //     />  
-    //     <button type="submit">Send</button>  
-    //   </form>  
-    //   <ul>  
-    //     {messages.map((message) => (  
-    //       <li key={message.id}>  
-    //         {message.user_id}: {message.message}  
-    //       </li>  
-    //     ))}  
-    //   </ul>  
-    // </div>
-    
-
-
-    // <div style={{maxWidth: '400px', margin: 'auto'}}>  
-    // <div style={{background: '#f1f0f0', padding: '10px', borderBottom: '1px solid #ccc'}}>  
-    //     <h2 style={{margin: '0'}}>Chat</h2>  
-    // </div>  ''
-    // <div style={{height: '400px', overflowY: 'scroll'}}>  
-    //     {messages.map((message, index) => (  
-    //     <div key={index} style={{display: 'flex', justifyContent: message.user_id === 'Me' ? 'flex-end' : 'flex-start', margin: '10px'}}>  
-    //         <div style={{background: message.user_id === 'Me' ? '#DCF8C6' : '#fff', padding: '10px', borderRadius: '10px', maxWidth: '70%'}}>  
-    //         {message.message}  
-    //         </div>  
-    //     </div>  
-    //     ))}  
-    // </div>  
-    // <form onSubmit={handleSubmit} style={{display: 'flex', marginTop: '10px'}}>  
-    //     <input type="text" value={newMessage} onChange={(event) => setNewMessage(event.target.value)} style={{flex: '1', padding: '10px'}} />  
-    //     <button type="submit" style={{background: '#4CAF50', color: '#fff', padding: '10px', border: 'none', borderRadius: '0 10px 10px 0'}}>Send</button>  
-    // </form>  
-    // </div> 
     <div style={{width: '100%', height: '100vh', backgroundColor: '#fff'}}>  
     <div style={{maxWidth: '600px', margin: '0 auto', height: '100%', display: 'flex', flexDirection: 'column'}}>  
         <div style={{background: '#f1f0f0', padding: '10px', borderBottom: '1px solid #ccc'}}>  
