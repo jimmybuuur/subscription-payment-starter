3908efe429b6bf1227be6be0d2e6c5ef077986c0
jimmybuuur <burkleyj122@gmail.com>
2023-05-11 15:40:58 -0700

    chat message

--- components/chat/ChatMessage.tsx
+++ components/chat/ChatMessage.tsx
@@ -12,7 +12,7 @@ export const ChatMessage: FC<Props> = ({ message }) => {
         className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-neutral-900" : "bg-blue-500 text-white"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
         style={{ overflowWrap: "anywhere" }}
       >
-        {message.content}
+        {message.message}
       </div>
     </div>
   );
