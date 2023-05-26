d8dff8fde0279207b1f0a81549ffabe16fc57e52
jimmybuuur <burkleyj122@gmail.com>
2023-05-25 20:22:42 -0700

    sidebar component

--- components/sidebar/ExtractsSidebar.tsx
+++ components/sidebar/ExtractsSidebar.tsx
@@ -5,14 +5,17 @@ interface Props {
 }  
   
 export const ExtractsSidebar: React.FC<Props> = ({ extracts }) => {  
-  return (  
-    <div className="w-full sm:w-3/5 h-screen bg-gray-100 p-4 overflow-auto">  
-      <h2 className="text-lg font-medium mb-2">Extracts</h2>  
-      <ul className="max-h-screen">  
-        {extracts.map((extract, index) => (  
-          <li key={index} className="mb-2 whitespace-pre-wrap">{extract}</li>  
-        ))}  
-      </ul>  
-    </div>  
-  );  
-};  
+    return (  
+      <div className="w-full sm:w-3/5 h-screen bg-gray-100 p-4 overflow-auto">  
+        <h2 className="text-lg font-medium mb-2">Extracts</h2>  
+        <ul className="max-h-screen">  
+          {extracts.length > 0 ? (  
+            <li className="mb-2 whitespace-pre-wrap">{extracts[extracts.length - 1]}</li>  
+          ) : (  
+            <li className="mb-2 whitespace-pre-wrap">No extracts yet</li>  
+          )}  
+        </ul>  
+      </div>  
+    );  
+  };  
+  
