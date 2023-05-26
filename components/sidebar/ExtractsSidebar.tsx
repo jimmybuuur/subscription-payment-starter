fd08d6b9e35b8e1c0bf2b43aff7c78d76a76bd1f
jimmybuuur <burkleyj122@gmail.com>
2023-05-25 19:57:04 -0700

    sidebar component

--- components/sidebar/ExtractsSidebar.tsx
+++ components/sidebar/ExtractsSidebar.tsx
@@ -6,7 +6,7 @@ interface Props {
   
 export const ExtractsSidebar: React.FC<Props> = ({ extracts }) => {  
   return (  
-    <div className="w-full sm:w-1/4 h-screen bg-gray-100 p-4">  
+    <div className="w-full sm:w-1/4 h-screen bg-gray-100 p-4 overflow-auto">  
       <h2 className="text-lg font-medium mb-2">Extracts</h2>  
       <ul className="max-h-screen">  
         {extracts.map((extract, index) => (  
